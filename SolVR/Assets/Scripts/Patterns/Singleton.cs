d1e197ac3f605a2970a19ef56e70f2f31e269d4a
wojwenc <wojwen@wp.pl>
2021-07-29 14:07:11 +0200

    Add singleton class

---
+++ SolVR/Assets/Scripts/Patterns/Singleton.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Patterns
+{
+    public class Singleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+        private static bool _shuttingDown = false;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_shuttingDown) return null;
+
+                if (_instance != null) return _instance;
+
+                _instance = FindObjectOfType<T>();
+                if (_instance != null) return _instance;
+
+                var obj = new GameObject {name = typeof(T).Name};
+                _instance = obj.AddComponent<T>();
+                return _instance;
+            }
+        }
+
+        public virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            _shuttingDown = true;
+        }
+        
+        private void OnDestroy()
+        {
+            _shuttingDown = true;
+        }
+    }
+}
