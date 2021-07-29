9b5818de763331e8ae470b366ffef4186591a3bb
wojwenc <wojwen@wp.pl>
2021-07-29 16:04:18 +0200

    Add dev and _preload scenes

---
+++ SolVR/Assets/Scripts/PreloadSceneHandler.cs
@@ -0,0 +1,12 @@
+﻿using Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadSceneHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        CustomSceneManager.Instance.QueueLoadScene("DevScene");
+    }
+}
