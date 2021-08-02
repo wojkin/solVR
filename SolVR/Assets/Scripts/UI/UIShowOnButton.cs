8f73c1cda0ebe44eac9721132a835a470e671864
kingapili <kinga.pil@wp.pl>
2021-08-02 12:51:12 +0200

    Add a class for handling input

--- SolVR/Assets/Scripts/UI/UIShowOnButton.cs
+++ SolVR/Assets/Scripts/UI/UIShowOnButton.cs
@@ -1,3 +1,5 @@
+using System;
+using InputHandlers;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,44 +9,48 @@ namespace UI
     /// Class responsible for showing and hiding the UIElement on a specified button click in front of the main camera.
     /// </summary>
     [RequireComponent(typeof(UIElement))]
+    [RequireComponent(typeof(EventOnButton))]
     public class UIShowOnButton : MonoBehaviour
     {
         private UIElement _uiElement; // UIElement attached to the gameObject
 
-        [Tooltip("Oculus controller button used to show and hide the UIElement.")]
-        [SerializeField] 
-        public OVRInput.Button button;
-        
-        [Tooltip("Offset between the player and the UI object.")] 
-        [SerializeField]
+        private EventOnButton _eventOnButton; // EventOnButton responsible for toggling UI on specific button
+
+        [Tooltip("Offset between the player and the UI object.")] [SerializeField]
         private float spawnOffset;
 
-        [Tooltip("The main camera object in front of which the UI object will be displayed.")] 
-        [SerializeField]
+        [Tooltip("The main camera object in front of which the UI object will be displayed.")] [SerializeField]
         private new Transform camera;
 
         /// <summary>
-        /// Initialize fields.
+        /// Initialize fields and add a listener for button input.
         /// </summary>
         private void Start()
         {
             _uiElement = GetComponent<UIElement>();
+            _eventOnButton = GetComponent<EventOnButton>();
+            _eventOnButton.afterInput.AddListener(VisibilityToggle);
         }
-        
+
         /// <summary>
-        /// Makes the gameObject face the camera when it's visible and manages hiding and showing the gameObject on a
-        /// button press.
+        /// Remove listener for button input.
         /// </summary>
-        private void Update()
+        private void OnDisable()
         {
-            // if the button is pressed show or hide the gameObject
-            if (OVRInput.GetUp(button))
-                if (_uiElement.IsVisible)
-                    Hide();
-                else
-                    Show();
+            _eventOnButton.afterInput.RemoveListener(VisibilityToggle);
         }
-        
+
+        /// <summary>
+        /// Manages toggling the visibility of the UIElement.
+        /// </summary>
+        private void VisibilityToggle()
+        {
+            if (_uiElement.IsVisible)
+                Hide();
+            else
+                Show();
+        }
+
         /// <summary>
         /// Makes the gameObject not visible.
         /// </summary>
@@ -60,7 +66,7 @@ namespace UI
         {
             if (camera == null)
                 camera = Camera.main.gameObject.transform; // find the main camera object if it's null
-            
+
             // calculate the position for the gameObject in front of the player
             transform.position = camera.position + camera.forward * spawnOffset;
 
