956110955619ca1bb383f39cfd4dcebd720a34f5
kingapili <kinga.pil@wp.pl>
2021-08-06 13:06:14 +0200

    Fix facing camera UI component when game is paused

--- SolVR/Assets/Scripts/UI/UIFaceCamera.cs
+++ SolVR/Assets/Scripts/UI/UIFaceCamera.cs
@@ -1,3 +1,4 @@
+using Managers;
 using UnityEngine;
 
 namespace UI
@@ -9,11 +10,10 @@ namespace UI
     public class UIFaceCamera : MonoBehaviour
     {
         private UIElement _uiElement; // UIElement attached to the gameObject
-        
-        [Tooltip("The camera the gameObject should face.")] 
-        [SerializeField]
+
+        [Tooltip("The camera the gameObject should face.")] [SerializeField]
         private new Transform camera;
-        
+
         /// <summary>
         /// Initialize fields.
         /// </summary>
@@ -21,18 +21,19 @@ namespace UI
         {
             _uiElement = GetComponent<UIElement>();
         }
-        
+
         /// <summary>
         /// Rotates the gameObject to always face the player. Interpolates the rotation in order to make it smoother.
         /// </summary>
         private void Update()
         {
-            if (!_uiElement.IsVisible)
+            // not visible or not interactable on pause
+            if (!_uiElement.IsVisible || (GameManager.gameIsPaused && !_uiElement.interactableOnPause))
                 return;
-            
+
             if (camera == null)
                 camera = Camera.main.gameObject.transform; // find the main camera object if it's null
-            
+
             // calculate the direction vector from the camera to the gameObject and ignore the vertical difference
             var lookPos = transform.position - camera.position;
             lookPos.y = 0;
@@ -41,8 +42,7 @@ namespace UI
             var rotation = Quaternion.LookRotation(lookPos);
 
             // interpolate between the gameObject rotation and the desired one
-            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 100f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.unscaledDeltaTime * 100f);
         }
-
     }
 }
