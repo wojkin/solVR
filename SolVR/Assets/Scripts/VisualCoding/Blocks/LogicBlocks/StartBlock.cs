97a26f56108040e9681a3e8ddd2e21761771c4a3
kingapili <kinga.pil@wp.pl>
2021-09-07 16:49:48 +0200

    SVR-77 Create start code block

---
+++ SolVR/Assets/Scripts/VisualCoding/Blocks/LogicBlocks/StartBlock.cs
@@ -0,0 +1,9 @@
+namespace VisualCoding.Blocks.LogicBlocks
+{
+    /// <summary>
+    /// Class representing a block that is at the beginning of the script.
+    /// </summary>
+    public class StartBlock : Block
+    {
+    }
+}
