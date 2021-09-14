ab3bb1a428d64b1d0a7b1b7fed3ffc023c39f4dd
kingapili <kinga.pil@wp.pl>
2021-09-14 13:02:02 +0200

    Creaate classes for handling dropdowns with operations

---
+++ SolVR/Assets/Scripts/UI/Dropdown/RelationalOperationDropdown.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using VisualCoding.Values.Enums;
+
+namespace UI.Dropdown
+{
+    /// <summary>
+    /// Class handling dropdown that corresponds to a <c>RelationalOperation</c> enum.
+    /// </summary>
+    public class RelationalOperationDropdown : EnumDropdown<RelationalOperation>
+    {
+        /// <summary>
+        /// Populates dropdown with corresponding operators of the enum values.
+        /// </summary>
+        protected override void PopulateDropdown()
+        {
+            var enumValues = Enum.GetValues(typeof(RelationalOperation));
+            var operators = new List<string>();
+            foreach (RelationalOperation enumValue in enumValues)
+            {
+                operators.Add(enumValue.GetRelationalOperator());
+            }
+            dropdown.AddOptions(operators);
+        }
+    }
+}
