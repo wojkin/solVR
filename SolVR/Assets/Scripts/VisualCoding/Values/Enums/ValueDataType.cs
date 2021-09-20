0383fc4be206a309475c02e411810e880aa11712
kingapili <kinga.pil@wp.pl>
2021-09-20 12:16:44 +0200

    Create enum for value data type and dropdown class to it.

---
+++ SolVR/Assets/Scripts/VisualCoding/Values/Enums/ValueDataType.cs
@@ -0,0 +1,10 @@
+namespace VisualCoding.Values.Enums
+{
+    /// <summary>
+    /// Enum that represents data types for value.
+    /// </summary>
+    public enum ValueDataType
+    {
+        Integer, Float, String, Boolean
+    }
+}
