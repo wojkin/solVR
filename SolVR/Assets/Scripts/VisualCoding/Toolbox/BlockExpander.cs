8541bc556e66693b0cbe22cfb202b7adcd74c857
wojwen <wojwenc@gmail.com>
2021-11-01 16:37:06 +0100

    Add component for expanding placed block

---
+++ SolVR/Assets/Scripts/VisualCoding/Toolbox/BlockExpander.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace VisualCoding.Toolbox
+{
+    public class BlockExpander : MonoBehaviour
+    {
+        #region Variables
+
+        /// <summary>Multiplier for leering block's scale.</summary>
+        private const float LerpFactor = 20f;
+        /// <summary>If the difference between current and target scale is below this threshold, the scale will be
+        /// instantly set to one.</summary>
+        private const float LerpThreshold = 0.001f;
+
+        #endregion
+
+        #region Built-in Methods
+
+        /// <summary>
+        /// Smoothly expands a block until it reaches a scale of one.
+        /// </summary>
+        void Update()
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, LerpFactor * Time.deltaTime);
+            if (1f - transform.localScale.x < LerpThreshold)
+            {
+                transform.localScale = Vector3.one;
+                Destroy(this); // destroy this component as it's no longer needed
+            }
+        }
+
+        #endregion
+    }
+}
