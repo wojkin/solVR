333a66b4aa6994b83adb077f11ba85989edc6db6
wojwen <wojwenc@gmail.com>
2021-11-11 15:03:54 +0100

    Add script for displaying current for loop iterations

---
+++ SolVR/Assets/Scripts/VisualCoding/Blocks/UI/DisplayCurrentIterations.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+using VisualCoding.Blocks.LogicBlocks.Loop;
+
+namespace VisualCoding.Blocks.UI
+{
+    /// <summary>
+    /// Component for displaying <see cref="ForBlock"/> iteration count on a TMP text filed.
+    /// </summary>
+    public class DisplayCurrentIterations : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        /// <summary>
+        /// Block which iteration count will be displayed.
+        /// </summary>
+        [SerializeField]
+        private ForBlock forBlock;
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>
+        /// Text for displaying the iteration count.
+        /// </summary>
+        private TextMeshProUGUI _iterationsText;
+
+        #endregion
+
+        #region Built-in Methods
+
+        /// <summary>
+        /// Initializes the text field component.
+        /// </summary>
+        private void Start()
+        {
+            _iterationsText = GetComponent<TextMeshProUGUI>();
+        }
+
+        /// <summary>
+        /// Registers handler for an event raised when iteration count changes.
+        /// </summary>
+        private void OnEnable()
+        {
+            forBlock.IterationChanged += UpdateText;
+        }
+
+        /// <summary>
+        /// Removes handler for an event raised when iteration count changes.
+        /// </summary>
+        private void OnDisable()
+        {
+            forBlock.IterationChanged -= UpdateText;
+        }
+
+        #endregion
+
+        #region Custom Methods
+
+        /// <summary>
+        /// Updates the text field with the iteration count value.
+        /// </summary>
+        /// <param name="iteration">New iteration count.</param>
+        private void UpdateText(int iteration)
+        {
+            _iterationsText.text = iteration.ToString();
+        }
+
+        #endregion
+    }
+}
