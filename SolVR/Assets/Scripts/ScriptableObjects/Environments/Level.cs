5c848db9a90f202de4f85fd53d4ee6889900fcc1
wojwen <wojwenc@gmail.com>
2021-11-17 16:16:03 +0100

    Add blocks to level SO

--- SolVR/Assets/Scripts/ScriptableObjects/Environments/Level.cs
+++ SolVR/Assets/Scripts/ScriptableObjects/Environments/Level.cs
@@ -1,21 +1,24 @@
+using System.Collections.Generic;
 using ScriptableObjects.TaskDescriptions;
 using UnityEngine;
 
 namespace ScriptableObjects.Environments
 {
     /// <summary>
-    /// Stores information about a level as scriptable object.
+    /// Stores information about a level as a scriptable object.
     /// </summary>
     [CreateAssetMenu(fileName = "Level", menuName = "ScriptableObjects/Environment/Level", order = 1)]
     public class Level : Environment
     {
         #region Serialized Fields
 
-        /// <summary>
-        /// Description of task on the level.
-        /// </summary>
+        /// <summary>Description of task on the level.</summary>
         [Tooltip("Description of task on the level.")]
         public TaskDescription levelTaskDescription;
+
+        /// <summary>List of blocks available in the level.</summary>
+        [Tooltip("Blocks available in the level.")]
+        public List<BlockData> blocks;
 
         #endregion
     }
