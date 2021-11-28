6d1012a48ee69fec83223ff5aadf099a297eb057
kingapili <kinga.pil@wp.pl>
2021-11-28 01:05:00 +0100

    Create task failure for one of collisions with ground invoked

---
+++ SolVR/Assets/Scripts/Tasks/TaskFailures/TaskFailureOnFall.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using Tasks.TaskFailures.Utils;
+using UnityEngine;
+
+namespace Tasks.TaskFailures
+{
+    /// <summary>
+    /// <inheritdoc/>
+    /// Class fails task if any of <see cref="boxCollisionsWithGround"/> will have collision with ground.
+    /// </summary>
+    public class TaskFailureOnFall : TaskFailure
+    {
+        #region Serialized Fields
+
+        /// <summary>List of objects that will invoke task failure if they collide with ground.</summary>
+        [SerializeField] private List<CollisionWithGround> boxCollisionsWithGround;
+
+        #endregion
+
+        #region Built-in Methods
+
+        /// <summary>
+        /// Subscribes to all needed events.
+        /// </summary>
+        private void OnEnable()
+        {
+            foreach (var collisionWithGround in boxCollisionsWithGround)
+                collisionWithGround.CollidedWithGround += OnFell;
+        }
+
+        /// <summary>
+        /// Unsubscribes from previously subscribed collision with ground event if task failure not failed.
+        /// </summary>
+        private void OnDisable()
+        {
+            if (!IsFailed) UnsubscribeFromCollisions();
+        }
+
+        #endregion
+
+        #region Custom Methods
+
+        /// <summary>
+        /// Calls <see cref="TaskFailure.OnFailed"/> and
+        /// unsubscribes from previously subscribed collision with ground event if task failure failed.
+        /// </summary>
+        private void OnFell()
+        {
+            OnFailed();
+            UnsubscribeFromCollisions(); // Only one object fell is needed to fail the task.
+        }
+
+        /// <summary>
+        /// Unsubscribes from previously subscribed collision with ground event.
+        /// </summary>
+        private void UnsubscribeFromCollisions()
+        {
+            foreach (var collisionWithGround in boxCollisionsWithGround)
+                collisionWithGround.CollidedWithGround -= OnFell;
+        }
+
+        #endregion
+    }
+}
