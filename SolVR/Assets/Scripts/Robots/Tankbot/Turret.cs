88778cb1e5723cbf07361f1d4690b5deb940c28c
wojwen <wojwenc@gmail.com>
2021-11-28 19:46:10 +0100

    Create turret component

---
+++ SolVR/Assets/Scripts/Robots/Tankbot/Turret.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Robots.Tankbot
+{
+    public class Turret : MonoBehaviour
+    {
+        #region Serialized Fields
+
+        /// <summary>Bullet prefab instantiated when the turret shoots.</summary>
+        [SerializeField] private GameObject bulletPrefab;
+
+        /// <summary>Spawn point for new bullets.</summary>
+        [SerializeField] private Transform bulletSpawnPoint;
+
+        #endregion
+
+        #region Variables
+
+        /// <summary>Force applied to bullets when shooting.</summary>
+        private const float BulletForce = 10f;
+        /// <summary>Rotation speed of the turret in degrees per second.</summary>
+        private const float RotationSpeed = 90f;
+        /// <summary>Tolerance for turret angle.</summary>
+        private const float AngleTolerance = 0.001f;
+
+        #endregion
+
+        #region Custom Methods
+
+        /// <summary>
+        /// Coroutine for rotating the turret to an angle.
+        /// </summary>
+        /// <param name="targetAngle">Angle the turret will be rotated to.</param>
+        /// <returns><see cref="IEnumerator"/> required for a coroutine.</returns>
+        public IEnumerator Rotate(float targetAngle)
+        {
+            // calculate the sign for changing the angle (based on whether it should increase or decrease)
+            float directionSign = transform.localRotation.eulerAngles.z > targetAngle ? -1 : 1;
+
+            // rotate the turret until it reaches the target angle
+            while (Math.Abs(transform.localRotation.eulerAngles.z - targetAngle) > AngleTolerance)
+            {
+                // change the steer angle based on speed, but not overshoot
+                var angleChange = Mathf.Min(RotationSpeed * Time.deltaTime,
+                    Mathf.Abs(transform.localRotation.eulerAngles.z - targetAngle));
+                transform.Rotate(Vector3.forward, angleChange * directionSign, Space.Self);
+                yield return null;
+            }
+        }
+
+        /// <summary>
+        /// Shoots a bullet out of the turrets barrel.
+        /// </summary>
+        public void Shoot()
+        {
+            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+            bullet.GetComponent<Bullet>().Shoot(BulletForce);
+        }
+
+        #endregion
+    }
+}
