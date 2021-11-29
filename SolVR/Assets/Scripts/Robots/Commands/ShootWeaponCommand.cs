b0094d2cbc592561155e0623acdb7c0d182c5868
wojwen <wojwenc@gmail.com>
2021-11-29 14:25:09 +0100

    Create shoot weapon command

---
+++ SolVR/Assets/Scripts/Robots/Commands/ShootWeaponCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using Robots.Actions;
+
+namespace Robots.Commands
+{
+    /// <summary>
+    /// A class representing a command for shooting robot's weapon.
+    /// </summary>
+    public class ShootWeaponCommand : Command<IShootableWeapon>
+    {
+        #region Custom Methods
+
+        /// <summary>
+        /// A coroutine for executing the shoot weapon command on a robot.
+        /// </summary>
+        /// <param name="robot">The robot, which weapon will be shot.</param>
+        /// <returns><see cref="IEnumerator"/> required for a coroutine.</returns>
+        protected override IEnumerator Execute(IShootableWeapon robot)
+        {
+            yield return robot.ShootWeapon();
+        }
+
+        #endregion
+    }
+}
