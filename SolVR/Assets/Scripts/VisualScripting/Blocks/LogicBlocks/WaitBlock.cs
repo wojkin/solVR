651ac29d00fd6105253318ab2a9a0a692e97c4c3
wojwen <wojwenc@gmail.com>
2021-11-29 19:57:49 +0100

    Create wait block component

---
+++ SolVR/Assets/Scripts/VisualScripting/Blocks/LogicBlocks/WaitBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Utils.ValueInRange;
+
+namespace VisualScripting.Blocks.LogicBlocks
+{
+    public class WaitBlock : Block
+    {
+        /// <summary>Number of seconds move command is executed.</summary>
+        [SerializeField] [Tooltip("Number of seconds an execution thread should wait for.")]
+        private ValueInRange<float> time;
+        
+        
+        /// <summary><inheritdoc cref="time"/></summary>
+        public float Time
+        {
+            get => time.Value;
+            set => time.Value = value;
+        }
+    }
+}
