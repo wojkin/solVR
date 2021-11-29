2b76a03f200af6cf8fd0ab707178f302d702f12f
kingapili <kinga.pil@wp.pl>
2021-11-29 22:33:26 +0100

    Configure wait block prefab

--- SolVR/Assets/Scripts/VisualScripting/Blocks/LogicBlocks/WaitBlock.cs
+++ SolVR/Assets/Scripts/VisualScripting/Blocks/LogicBlocks/WaitBlock.cs
@@ -5,16 +5,35 @@ namespace VisualScripting.Blocks.LogicBlocks
 {
     public class WaitBlock : Block
     {
+        #region Serialized Fields
+
         /// <summary>Number of seconds move command is executed.</summary>
         [SerializeField] [Tooltip("Number of seconds an execution thread should wait for.")]
         private ValueInRange<float> time;
-        
-        
+
+        #endregion
+
+        #region Variables
+
         /// <summary><inheritdoc cref="time"/></summary>
         public float Time
         {
             get => time.Value;
             set => time.Value = value;
         }
+
+        #endregion
+
+        #region Built-in Methods
+
+        /// <summary>
+        /// Initialize fields.
+        /// </summary>
+        private void Start()
+        {
+            time.Initialize();
+        }
+
+        #endregion
     }
 }
